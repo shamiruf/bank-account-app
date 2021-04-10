0dede2c75b722306892ab24cd059423267e1cf6e
Rufi <43849574+shamiruf@users.noreply.github.com>
2021-04-10 16:59:17 +0200

    new data

--- script.js
+++ script.js
@@ -4,36 +4,48 @@
 /////////////////////////////////////////////////
 // BANK ACCOUNT APP
 
-// Data
+// Data Contains movement dates, currency and locale
 const account1 = {
-  owner: 'Alex Beroun',
-  movements: [150, 400, -300, 2020, -700, -240, 150, 2300],
-  interestRate: 1.3, // %
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
-  owner: 'Sofi Beridze',
-  movements: [2000, 340, -120, -320, -5600, -300, 7600, -40],
-  interestRate: 1.7,
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Mari Novosad',
-  movements: [100, -540, 280, -30, -100, 2000, 500, -200],
-  interestRate: 0.3,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Jim Smith',
-  movements: [500, 1200, -300, 40, 250],
-  interestRate: 0.9,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
